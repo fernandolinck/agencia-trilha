1bd38698c2e48cd887f23fe7b599d0cfaefd0f2e
fernandolinck <fernandovourgh@gmail.com>
2021-04-12 21:46:21 -0300

    Accordion js ready
    Accordion js ready

--- assets/js/main.js
+++ assets/js/main.js
@@ -52,11 +52,49 @@ window.addEventListener('scroll', scrollTop)
 /* Accordion */
 document.querySelectorAll('.processo__accordion-passo').forEach((item) => {
     item.addEventListener('click', (event) => {
+        let accCollapse = item.nextElementSibling;
+
+        if (!item.classList.contains('collapsing')){
+            
+        }
+
+        // Open accordion item
+        if (!item.classList.contains('open')){
+            // Remove "collapse" and add "collapsing" class to .processo__collapse (sibling)
+
+            accCollapse.style.display = 'block';
+            let accHeight = accCollapse.clientHeight;
+
+            setTimeout (() => {
+                accCollapse.style.height = accHeight + 'px';
+                accCollapse.style.display = '';
+            }, 1);
+
+            accCollapse.classList = 'processo__collapse collapsing';
+
+            // After x amount of time, Remove "collapsing" class and add "open" class
+             setTimeout(() => {
+                accCollapse.classList = 'processo__collapse collapse open';
+            }, 300);
+        }
+
+        // Close accordion item
+        else {
+            // Remove "collapse open" from .processo__collapse, add "collapsing"
+            accCollapse.classList = 'processo__collapse collapsing';
+           
+
+            setTimeout(() => {
+                accCollapse.style.height = '0px';
+            }, 1);
+
+
+            setTimeout(() => {
+                accCollapse.classList = 'processo__collapse collapse';
+                accCollapse.style.height = '';
+            }, 300);
+        }
+    
         item.classList.toggle('open');
-
-        // Add "collapsing" class to .processo__collapse (sibling)
-
-       // Remove "collapsing" class and add "open" class
-        item.nextElementSibling.classList.toggle('open');
     });
 });
