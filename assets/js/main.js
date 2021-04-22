76aaecc4e23d1968910fd3331d463ba5a5e3d835
fernandolinck <fernandovourgh@gmail.com>
2021-04-22 19:36:00 -0300

    d
    d

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,6 +8,7 @@ const showMenu = (toggleId, navId) => {
         toggle.addEventListener('click', () => {
             // We add the show-menu class to the div tag with the nav__menu class
             nav.classList.toggle('show-menu')
+            toggle.classList.toggle('toggle-color')
         })
     }
 }
