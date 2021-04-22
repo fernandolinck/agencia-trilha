b20b78df97677948893e253c7a5b5ad7df884ec7
fernandolinck <fernandovourgh@gmail.com>
2021-04-21 21:31:40 -0300

    kk
    kk

--- assets/js/main.js
+++ assets/js/main.js
@@ -108,22 +108,31 @@ var swiper = new Swiper('.swiper-container', {
     grabCursor: true,
     loop: true,
     pagination: {
-      el: '.swiper-pagination',
-      clickable: true,
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
     },
     breakpoints: {
+        100: {
+            slidesPerView: 1,
+            spaceBetween: 20,
+        },
         640: {
-          slidesPerView: 2,
-          spaceBetween: 20,
+            slidesPerView: 2,
+            spaceBetween: 20,
         },
         768: {
-          slidesPerView: 2,
-          spaceBetween: 30,
+            slidesPerView: 2,
+            spaceBetween: 30,
         },
         1024: {
-          slidesPerView: 3,
-          spaceBetween: 30,
+            slidesPerView: 3,
+            spaceBetween: 30,
         },
-      }
-  });
+    }
+});
+
 
